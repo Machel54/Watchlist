086bd8f7040085c6fab6d5d7b0e41609e1ff6cb6
Machel Nyanumba <machelnyanumba14@gmail.comgit config --global user.email machelnyanumba14@gmail.com>
2019-10-18 15:02:08 +0300

    add flask-migrate

---
+++ config.py
@@ -0,0 +1,21 @@
+import os
+
+class Config:
+
+    MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
+    MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://machel:jkl@localhost/watchlist'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class ProdConfig(Config):
+    pass
+
+
+class DevConfig(Config):
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
---
+++ start.sh
@@ -0,0 +1,4 @@
+export MOVIE_API_KEY='500a7fe7099d491bc80e510c16a7344a'
+export SECRET_KEY='KKKKK'
+
+python3.6 manage.py server
